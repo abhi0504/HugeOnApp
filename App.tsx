8b857b600e4d4e5d5706cac3ea3d9d8a8fee85ef
unknown <sharmaash876@gmail.com>
2023-07-23 14:13:34 +0530

    new commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
+});
+
+export default App;
